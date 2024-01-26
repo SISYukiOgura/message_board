7caeb0275388f4b09408575450ae84bbe3b6fb50
yuuki.ogura2 <Yuki_Ogura@sis2.saison.co.jp>
2024-01-26 15:16:41 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,10 +7,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+
+
+
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "messages")
+
+
 public class Message {
     @Id
     @Column(name = "id")
@@ -69,3 +82,4 @@ public class Message {
         this.updated_at = updated_at;
     }
 }
+
